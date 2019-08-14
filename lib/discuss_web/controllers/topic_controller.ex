6ebe8fee09dddd0955e7f8ab96092ccff3caaa25
bmroby01 <brianna124@gmail.com>
2019-08-14 13:58:43 -0400

    Added create and started edit

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,14 +2,37 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias DiscussWeb.Topic
+  alias Discuss.Repo
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{}, params)
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
-    
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
   end
 end
