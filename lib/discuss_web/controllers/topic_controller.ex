ff9ac6b0f41ca2681f91d3e01844b5f8642f052d
bmroby01 <brianna124@gmail.com>
2019-08-23 09:40:32 -0400

    Added user_id to schema for topic ownership

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,5 +1,6 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  use Ecto.Schema
 
   alias DiscussWeb.Topic
   alias Discuss.Repo
@@ -19,7 +20,10 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset =
+      conn.assigns.user
+      |> Ecto.build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
